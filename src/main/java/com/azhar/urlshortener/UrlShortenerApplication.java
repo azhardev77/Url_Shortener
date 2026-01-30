1f52cb1b8caddf4cd8777d0d419f7d240e2c64c3
azhar <azharchat7793@gmail.com>
2026-01-30 16:12:16 +0530

    Fix Railway

--- src/main/java/com/azhar/urlshortener/UrlShortenerApplication.java
+++ src/main/java/com/azhar/urlshortener/UrlShortenerApplication.java
@@ -2,9 +2,12 @@ package com.azhar.urlshortener;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class UrlShortenerApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(UrlShortenerApplication.class, args);
     }
